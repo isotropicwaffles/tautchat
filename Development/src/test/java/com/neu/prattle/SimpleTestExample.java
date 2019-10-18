db183b7216517d0dfaab69a8a2320ff28af5615a
Richard Alexander Showalter-Bucher <showalter-bucher.r@husky.neu.edu>
2019-10-17 21:52:47 -0400

    Added initial tests of prattle
    Still need tests for ChatEndPoint.java

--- Development/src/test/java/com/neu/prattle/SimpleTestExample.java
+++ Development/src/test/java/com/neu/prattle/SimpleTestExample.java
@@ -2,15 +2,31 @@ package com.neu.prattle;
 
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
+import java.util.Set;
 
 import com.neu.prattle.service.UserService;
 import com.neu.prattle.service.UserServiceImpl;
+import com.neu.prattle.websocket.ChatEndpoint;
+import com.neu.prattle.websocket.MessageDecoder;
+import com.neu.prattle.websocket.MessageEncoder;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.neu.prattle.controller.UserController;
+import com.neu.prattle.main.PrattleApplication;
+import com.neu.prattle.model.Message;
 import com.neu.prattle.model.User;
+
+import javax.websocket.EncodeException;
+import javax.websocket.Session;
+
+import javax.ws.rs.core.Response;
+import javax.websocket.EndpointConfig;
 
 public class SimpleTestExample {
 
@@ -34,6 +50,99 @@ public class SimpleTestExample {
 		Optional<User> user = as.findUserByName("Mike");
 		assertTrue(user.isPresent());
 	}
+	
+	// This method just tries to create an empty user and set the name 
+	@Test
+	public void emptyUserConstructorTest(){
+		User user = new User();
+		user.setName("John");
+		assertEquals("John",user.getName());
+	}
+	
+	// This method to sets that user.equals is false if fed a non User object 
+	@Test
+	public void notEqualObjectTest(){
+		User user = new User();
+		user.setName("John");
+		assertFalse(user.equals(new String()));
+	}
+	
+	// This method tests the case where multiple users of the same name are created via the user controller
+	@Test
+	public void createUserConterollerFailTest(){
+		UserController controller = new UserController();
+		Response okayStatus= Response.ok().build();
+		Response failure409 = Response.status(409).build();
+				
+		controller.createUserAccount(new User("Jack"));
+		
+		assertEquals(failure409.getStatus(), controller.createUserAccount(new User("Jack")).getStatus());
+	}
+	
+	// This method tests if a unique user is added via the user controller
+	@Test
+	public void createUserConterollerSuccessTest(){
+		UserController controller = new UserController();
+		Response okayStatus= Response.ok().build();
+				
+		
+		assertEquals(okayStatus.getStatus(), controller.createUserAccount(new User("Tim")).getStatus());
+	}
+	
+	// This method tests encoding and decoding a message
+	@Test
+	public void encodeDecodeTest() throws EncodeException{
+		MessageDecoder decoder = new MessageDecoder();
+		MessageEncoder encoder = new MessageEncoder();
+		Message simpleMessage = Message.messageBuilder()
+        .setFrom("Tom")
+        .setTo("Mark")
+        .setMessageContent("Simple")
+        .build();
+		
+		String encodedMessage = encoder.encode(simpleMessage);
+		
+		Message decodedMessage = decoder.decode(encodedMessage);
+		
+		encoder.destroy();
+		decoder.destroy();
+		assertEquals(simpleMessage.getContent(),decodedMessage.getContent());
+		assertEquals(simpleMessage.getFrom(),decodedMessage.getFrom());
+		assertEquals(simpleMessage.getTo(),decodedMessage.getTo());
+		assertEquals(simpleMessage.toString(),decodedMessage.toString());
+
+	}
+	
+	
+	// This method if decode check works
+	@Test
+	public void willDecodeTest(){
+		MessageDecoder decoder = new MessageDecoder();
+				
+		assertFalse(decoder.willDecode(null));
+		assertTrue(decoder.willDecode("test"));
+
+	}
+	
+	// This method tests getClasses function of PrattleApplication
+	@Test
+	public void prattleAppGetClassesTest(){
+		PrattleApplication prattleApp = new PrattleApplication();
+		Set<Class<?>> resourceClasses = prattleApp.getClasses();
+		
+		assertEquals(1,resourceClasses.size());
+		assertEquals(UserController.class, resourceClasses.iterator().next());
+
+	}
+	
+	// This method tests getClasses function
+	@Test
+	public void endPointChatTest() throws EncodeException{
+		ChatEndpoint chat = new ChatEndpoint();
+
+
+	}
+	
 	
 	// Performance testing to benchmark our number of users that can be added 
 	// in 1 sec	
