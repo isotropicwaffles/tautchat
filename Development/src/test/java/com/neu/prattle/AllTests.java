dae8ed532f0e0b6cf2edad434ccd04304fa1ff2e
Alex Showalter-Bucher <rasb@ll.mit.edu>
2019-10-18 10:47:58 -0400

    Adding AllTest file
    This hopefully will fix the issue with sonarqube detecting the tests

---
+++ Development/src/test/java/com/neu/prattle/AllTests.java
@@ -0,0 +1,11 @@
+package SimpleTestExample.java
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SimpleTestExample.class})
+public class AllTests {
+
+}
