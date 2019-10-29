be389c06328df52beb59eab93bde3b7f7fdb31fa
Richard Alexander Showalter-Bucher <showalter-bucher.r@husky.neu.edu>
2019-10-29 00:36:27 -0400

    Initial Implementation of Sentiment Analysis Methods Completed

---
+++ Development/src/main/java/com/neu/textprocessing/Sentiment.java
@@ -0,0 +1,13 @@
+package com.neu.textprocessing;
+
+/**
+* This class represents an enumerations of sentiments 
+*
+* @author Richard Alexander Showalter-Bucher
+* @version 1.0 10/29/2019
+*/
+public enum Sentiment {
+	POSITIVE,
+	NEGATIVE,
+	NEUTRAL,
+}
---
+++ Development/src/test/java/com/neu/textprocessing/RunAllTests.java
@@ -0,0 +1,11 @@
+package com.neu.textprocessing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SentimentTest.class})
+public class RunAllTests {
+
+}
