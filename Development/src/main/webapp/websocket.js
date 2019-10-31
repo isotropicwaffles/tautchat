5525a278ca77a9a91f2a18799eb093a29c2ae305
Richard Alexander Showalter-Bucher <showalter-bucher.r@husky.neu.edu>
2019-10-31 00:07:00 -0400

    Initial work on GUI for login/user creation.
    Layout completed. Some logic has been partially started.

--- Development/src/main/webapp/websocket.js
+++ Development/src/main/webapp/websocket.js
@@ -1,19 +1,22 @@
 var ws;
+var username;
+var error_label = document.getElementById("login_status");
 
 function connect() {
-    var username = document.getElementById("username").value;
-    
-    var host = document.location.host;
-    var pathname = document.location.pathname;
-    
-    ws = new WebSocket("ws://" +host  + pathname + "chat/" + username);
-
-    ws.onmessage = function(event) {
-    var log = document.getElementById("log");
-        console.log(event.data);
-        var message = JSON.parse(event.data);
-        log.innerHTML += message.from + " : " + message.content + "\n";
-    };
+	if (typeof ws === 'undefined') {
+				
+		var host = document.location.host;
+		var pathname = document.location.pathname;
+		
+		ws = new WebSocket("ws://" +host  + pathname + "chat/" + username);
+		
+		ws.onmessage = function(event) {
+			var log = document.getElementById("log");
+		    console.log(event.data);
+		    var message = JSON.parse(event.data);
+		    log.innerHTML += message.from + " : " + message.content + "\n";
+		};
+	}
 }
 
 function send() {
@@ -23,4 +26,46 @@ function send() {
     });
 
     ws.send(json);
+}
+
+function login() {
+	username = document.getElementById("username_login").value;
+
+	// Run this in case they are not already connected to server
+	connect();
+	
+    var json = JSON.stringify({
+        "content": "~USER_SERVICE-LOGIN: " + username
+    });
+    
+    ws.send(json);
+    
+    // If unsuccessful this should run
+    error_label.style.color = '#d00'
+	error_label.innerHTML =  "ERROR: User Name Does not Exist. Please Create User First."
+		
+	//If successful this should run
+	document.location.href='/prattle/chat.html';
+
+}
+
+function create_user() {
+	username = document.getElementById("username_login").value;
+
+	connect();
+
+    var json = JSON.stringify({
+        "content": "~USER_SERVICE-CREATION: " + username
+    });
+    
+    ws.send(json);
+    
+
+    // If unsuccessful this should run
+    error_label.style.color = '#d00'
+    error_label.innerHTML =  "ERROR: Could Not Create User Name. Please Try Another Name."
+    	
+   	//If successful this should run
+    error_label.style.color = '#d00'
+    error_label.innerHTML =  "User Successfully Created!"
 }
